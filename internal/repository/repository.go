609dcf3c3d3b8667c467bad75a5f2dca02006a64
alwi09 <alwiirfani1927@gmail.com>
2023-06-14 16:09:09 +0700

    docs: add some repository interface

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -4,4 +4,8 @@ import "cli_interactive/internal/model/entity"
 
 type WikisRepository interface {
 	AddTopicWikisRepo(wikis *entity.Wikis) error
+	GetAllWikisRepo() ([]*entity.Wikis, error)
+	GetWikisByIDRepo(id int) (*entity.Wikis, error)
+	UpdateWikisRepo(wikis *entity.Wikis) error
+	DeleteWikisRepo(id int) error
 }
