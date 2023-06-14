db49588905c26900bffeb5af325df8ce3500cd4b
alwi09 <alwiirfani1927@gmail.com>
2023-06-14 14:22:26 +0700

    docs: add repository implementation

--- internal/database/wikis.go
+++ internal/database/wikis.go
@@ -1,1 +1,21 @@
 package database
+
+import (
+	"cli_interactive/internal/model/entity"
+
+	"gorm.io/gorm"
+)
+
+type WikisRepositoryImpl struct {
+	DB *gorm.DB
+}
+
+func NewWikisRepository(DB *gorm.DB) *WikisRepositoryImpl {
+	return &WikisRepositoryImpl{
+		DB: DB,
+	}
+}
+
+func (repository *WikisRepositoryImpl) AddTopicWikisRepo(wikis *entity.Wikis) error {
+	return repository.DB.Create(wikis).Error
+}
