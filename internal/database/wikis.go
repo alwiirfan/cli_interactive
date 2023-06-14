04a28ce6914f31cf6844fbf4b20483d467652fe2
alwi09 <alwiirfani1927@gmail.com>
2023-06-14 16:08:23 +0700

    docs: add some function repository implementation

--- internal/database/wikis.go
+++ internal/database/wikis.go
@@ -19,3 +19,29 @@ func NewWikisRepository(DB *gorm.DB) *WikisRepositoryImpl {
 func (repository *WikisRepositoryImpl) AddTopicWikisRepo(wikis *entity.Wikis) error {
 	return repository.DB.Create(wikis).Error
 }
+
+func (repository *WikisRepositoryImpl) GetAllWikisRepo() ([]*entity.Wikis, error) {
+	var wikis []*entity.Wikis
+	err := repository.DB.Find(&wikis).Error
+	if err != nil {
+		return nil, err
+	}
+	return wikis, nil
+}
+
+func (repository *WikisRepositoryImpl) GetWikisByIDRepo(id int) (*entity.Wikis, error) {
+	wikis := &entity.Wikis{}
+	err := repository.DB.First(wikis, id).Error
+	if err != nil {
+		return nil, err
+	}
+	return wikis, nil
+}
+
+func (repository *WikisRepositoryImpl) UpdateWikisRepo(wikis *entity.Wikis) error {
+	return repository.DB.Save(wikis).Error
+}
+
+func (repository *WikisRepositoryImpl) DeleteWikisRepo(id int) error {
+	return repository.DB.Delete(&entity.Wikis{}, id).Error
+}
