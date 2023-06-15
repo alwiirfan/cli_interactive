1729893c24812d2a99dc92e1dd9e887ee490ec72
alwi09 <alwiirfani1927@gmail.com>
2023-06-15 17:15:03 +0700

    docs: add some handlers from repositories

--- internal/service/wikisHandler.go
+++ internal/service/wikisHandler.go
@@ -3,11 +3,14 @@ package service
 import (
 	"fmt"
 	"log"
+	"strconv"
 	"time"
 
 	"cli_interactive/internal/database"
+	"cli_interactive/internal/model/dto"
 	"cli_interactive/internal/model/entity"
 
+	"github.com/go-co-op/gocron"
 	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
 )
@@ -22,6 +25,7 @@ func NewHandlerImpl(repository *database.WikisRepositoryImpl) *HandlerImpl {
 	}
 }
 
+// menambahkan topik
 func (handler *HandlerImpl) StartInteractiveCLI(cmd *cobra.Command, args []string) {
 	prompt := promptui.Prompt{
 		Label: "Enter a topic",
@@ -50,6 +54,7 @@ func (handler *HandlerImpl) StartInteractiveCLI(cmd *cobra.Command, args []strin
 	}
 }
 
+// untuk mendapatkan semua data wiki dari repository
 func (handler *HandlerImpl) GetAllWikisHandler(cmd *cobra.Command, args []string) {
 	wikis, err := handler.wikisRepository.GetAllWikisRepo()
 	if err != nil {
@@ -61,13 +66,33 @@ func (handler *HandlerImpl) GetAllWikisHandler(cmd *cobra.Command, args []string
 	}
 }
 
+// Menggunakan prompt untuk meminta pengguna memasukkan ID
 func (handler *HandlerImpl) GetWikisByIDHandler(cmd *cobra.Command, args []string) {
+	var id string
 	if len(args) != 1 {
-		log.Fatal("Please provide a single argument: ID")
+		prompt := promptui.Prompt{
+			Label: "Enter an ID",
+			Validate: func(input string) error {
+				if input == "" {
+					return fmt.Errorf("ID cannot be empty")
+				}
+				return nil
+			},
+		}
+
+		var err error
+		id, err = prompt.Run()
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		id = args[0]
 	}
 
-	id := args[0]
-	wikiID := convertToInt(id)
+	wikiID, err := strconv.Atoi(id)
+	if err != nil {
+		log.Fatal("Invalid ID format:", err)
+	}
 
 	wiki, err := handler.wikisRepository.GetWikisByIDRepo(wikiID)
 	if err != nil {
@@ -77,13 +102,14 @@ func (handler *HandlerImpl) GetWikisByIDHandler(cmd *cobra.Command, args []strin
 	fmt.Printf("ID: %d, Topic: %s, Description: %s\n", wiki.ID, wiki.Topic, wiki.Description)
 }
 
-func (handler *HandlerImpl) UpdateWikisHandler(cmd *cobra.Command, args []string) {
+// Handler untuk mengambil input dari pengguna dan memperbarui topik dan deskripsi wiki
+func (handler *HandlerImpl) UpdateTopicDescriptionHandler(cmd *cobra.Command, args []string) {
 	if len(args) != 1 {
 		log.Fatal("Please provide a single argument: ID")
 	}
 
 	id := args[0]
-	wikiID := convertToInt(id)
+	wikiID := dto.ConvertToInt(id)
 
 	wiki, err := handler.wikisRepository.GetWikisByIDRepo(wikiID)
 	if err != nil {
@@ -93,7 +119,7 @@ func (handler *HandlerImpl) UpdateWikisHandler(cmd *cobra.Command, args []string
 	prompt := promptui.Prompt{
 		Label:    "Enter a new topic",
 		Default:  wiki.Topic,
-		Validate: validateNonEmptyInput,
+		Validate: dto.ValidateNonEmptyInput,
 	}
 
 	newTopic, err := prompt.Run()
@@ -101,10 +127,17 @@ func (handler *HandlerImpl) UpdateWikisHandler(cmd *cobra.Command, args []string
 		log.Fatal(err)
 	}
 
+	// Mengambil deskripsi baru dari Wikipedia berdasarkan topik baru
+	newDescription, err := handler.wikisRepository.UpdateDescriptionFromWikipedia(newTopic)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	wiki.Topic = newTopic
+	wiki.Description = newDescription
 	wiki.Updated_at = time.Now()
 
-	err = handler.wikisRepository.UpdateWikisRepo(wiki)
+	err = handler.wikisRepository.UpdateTopicDescriptionRepo(wiki)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -112,13 +145,14 @@ func (handler *HandlerImpl) UpdateWikisHandler(cmd *cobra.Command, args []string
 	fmt.Println("Wiki updated successfully.")
 }
 
+// untuk menghapus data wiki berdasarkan ID yang diberikan sebagai argumen.
 func (handler *HandlerImpl) DeleteWikisHandler(cmd *cobra.Command, args []string) {
 	if len(args) != 1 {
 		log.Fatal("Please provide a single argument: ID")
 	}
 
 	id := args[0]
-	wikiID := convertToInt(id)
+	wikiID := dto.ConvertToInt(id)
 
 	err := handler.wikisRepository.DeleteWikisRepo(wikiID)
 	if err != nil {
@@ -128,44 +162,51 @@ func (handler *HandlerImpl) DeleteWikisHandler(cmd *cobra.Command, args []string
 	fmt.Println("Wiki deleted successfully.")
 }
 
-func (handler *HandlerImpl) AddTopicHandler(cmd *cobra.Command, args []string) {
-	if len(args) != 1 {
-		log.Fatal("Please provide a single argument: Topic")
-	}
+func (handler *HandlerImpl) WorkerHandler(cmd *cobra.Command, args []string) {
+	// Membuat sebuah scheduler baru
+	scheduler := gocron.NewScheduler(time.UTC)
 
-	topic := args[0]
+	// Mengatur jadwal untuk menjalankan fungsi yang diberikan setiap 1 menit.
+	_, err := scheduler.Every(1).Minute().Do(func() {
 
-	wiki := &entity.Wikis{
-		ID:          0,
-		Topic:       topic,
-		Description: "",
-		Created_at:  time.Now(),
-		Updated_at:  time.Now(),
-	}
+		// untuk mendapatkan daftar wikis yang memiliki deskripsi kosong.
+		wikis, err := handler.wikisRepository.GetWikisWithEmptyDescriptionRepo()
+		if err != nil {
+			log.Println("failed to get wikis:", err)
+			return
+		}
 
-	err := handler.wikisRepository.AddTopicWikisRepo(wiki)
+		// Melakukan iterasi pada setiap wiki dalam daftar wikis.
+		for _, wiki := range wikis {
+
+			// Mencetak informasi ID, topik, dan deskripsi dari wiki saat ini menggunakan log.Printf().
+			log.Printf("ID: %d, Topic: %s, Description: %s\n", wiki.ID, wiki.Topic, wiki.Description)
+
+			// untuk mengupdate deskripsi wiki dari sumber Wikipedia.
+			description, err := handler.wikisRepository.UpdateDescriptionFromWikipedia(wiki.Topic)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+
+			// Mengupdate deskripsi wiki dengan nilai yang baru.
+			wiki.Description = description
+
+			// untuk mengupdate waktu terakhir diubah (updatedAt) dari wiki.
+			err = handler.wikisRepository.UpdateUpdatedAt(wiki)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+
+			log.Println("Description updated successfully.")
+		}
+	})
+
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("failed to schedule worker:", err)
 	}
 
-	fmt.Println("Topic saved to the database.")
-}
-
-// Helper function to convert string to int.
-func convertToInt(str string) int {
-	var val int
-	_, err := fmt.Sscanf(str, "%d", &val)
-	if err != nil {
-		log.Fatal("Invalid ID provided")
-	}
-
-	return val
-}
-
-// Helper function to validate non-empty input.
-func validateNonEmptyInput(input string) error {
-	if input == "" {
-		return fmt.Errorf("input must not be empty")
-	}
-	return nil
+	// Memulai scheduler dan menjalankan pekerjaan secara terjadwal.
+	scheduler.StartBlocking()
 }
