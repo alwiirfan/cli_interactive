2f38d0bdb351322466e5fd81833dc09a44e0d54d
alwi09 <alwiirfani1927@gmail.com>
2023-06-15 17:16:07 +0700

    docs: add func for helper to handler

---
+++ internal/model/dto/helper.go
@@ -0,0 +1,25 @@
+package dto
+
+import (
+	"fmt"
+	"log"
+)
+
+// pembantu untuk mengonversi string ke int.
+func ConvertToInt(str string) int {
+	var value int
+	_, err := fmt.Sscanf(str, "%d", &value)
+	if err != nil {
+		log.Fatal("Invalid ID provided")
+	}
+
+	return value
+}
+
+// pembantu untuk memvalidasi input yang tidak kosong.
+func ValidateNonEmptyInput(input string) error {
+	if input == "" {
+		return fmt.Errorf("input must not be empty")
+	}
+	return nil
+}
