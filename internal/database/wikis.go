9856f43fed4edb67ba19b0e5ea153d0c81925069
alwi09 <alwiirfani1927@gmail.com>
2023-06-15 17:13:22 +0700

    docs: add some repository implemetation

--- internal/database/wikis.go
+++ internal/database/wikis.go
@@ -2,7 +2,11 @@ package database
 
 import (
 	"cli_interactive/internal/model/entity"
+	"fmt"
+	"net/http"
+	"time"
 
+	"github.com/PuerkitoBio/goquery"
 	"gorm.io/gorm"
 )
 
@@ -38,10 +42,50 @@ func (repository *WikisRepositoryImpl) GetWikisByIDRepo(id int) (*entity.Wikis, 
 	return wikis, nil
 }
 
-func (repository *WikisRepositoryImpl) UpdateWikisRepo(wikis *entity.Wikis) error {
+func (repository *WikisRepositoryImpl) UpdateTopicDescriptionRepo(wikis *entity.Wikis) error {
 	return repository.DB.Save(wikis).Error
 }
 
 func (repository *WikisRepositoryImpl) DeleteWikisRepo(id int) error {
 	return repository.DB.Delete(&entity.Wikis{}, id).Error
+}
+
+func (repository *WikisRepositoryImpl) GetWikisWithEmptyDescriptionRepo() ([]*entity.Wikis, error) {
+	var wikis []*entity.Wikis
+	err := repository.DB.Where("description = '' OR description IS NULL").Find(&wikis).Error
+	if err != nil {
+		return nil, err
+	}
+	return wikis, nil
+}
+
+func (repository *WikisRepositoryImpl) UpdateDescriptionFromWikipedia(topic string) (string, error) {
+	url := fmt.Sprintf("https://id.wikipedia.org/wiki/%s", topic)
+
+	// Membuat HTTP GET request ke URL Wikipedia
+	response, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("failed to fetch Wikipedia page: %s", response.Status)
+	}
+
+	// Menggunakan goquery untuk memparsing response body
+	doc, err := goquery.NewDocumentFromReader(response.Body)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse HTML: %w", err)
+	}
+
+	// Mengambil paragraf pertama dari halaman Wikipedia
+	firstParagraph := doc.Find("div.mw-parser-output > p").First().Text()
+
+	return firstParagraph, nil
+}
+
+func (repository *WikisRepositoryImpl) UpdateUpdatedAt(wikis *entity.Wikis) error {
+	wikis.Updated_at = time.Now()
+	return repository.DB.Save(wikis).Error
 }
