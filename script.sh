18e0ab564a746ff97b1faed8d14c0602b6b863fb
Mohammad Alwi Irfani <alwi.irfani1927@gmail.com>
2023-06-21 14:27:28 +0700

    docs: slight changes

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 sudo docker build -t alwi09/cli-image .
 
 # run and create container to cli_interactive and create network
-sudo docker container run --name cli-container --network=host cli-image
+sudo docker container run --name cli-container --network=host alwi09/cli-image
 
 # 
 sudo docker container logs cli-container
